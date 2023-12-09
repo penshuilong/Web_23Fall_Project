9a87b2dc8274b011fecd897ce517a51c62613cd7
Ziyi An <ziyia2@uw.edu>
2023-12-09 15:10:11 -0500

    edit link for likes

--- src/project/likes/client.js
+++ src/project/likes/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = "https://project-web23.onrender.com/api";
 
 const USERS_API = `${API_BASE}/users`;
 const LIKES_API = `${API_BASE}/likes`;
