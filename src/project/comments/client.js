29db3184ad02a1472731a1d4fe98379bedc2686f
zihanzhao <zihanz@bu.edu>
2023-12-11 01:09:21 -0500

    comment

---
+++ src/project/comments/client.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:4000/api";
+
+const USERS_API = `${API_BASE}/users`;
+const COMMENTS_API = `${API_BASE}/comments`;
+
+export const findAllComments = async () => {};
+
+
+export const createComment = async (userId, idMeal, strMeal, strComments) => {
+  try {
+    const response = await axios.post(`${USERS_API}/${userId}/comments/${idMeal}`, {
+      idMeal,
+      strMeal,
+      strComments,
+      
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    throw error;
+  }
+};
+
+
+
+  
+export const deleteUserCommentsMeal = async (userId, idMeal) => {
+  const response = await axios.delete(`${USERS_API}/${userId}/comments/${idMeal}`);
+  return response.data;
+};
+
+export const findUsersThatCommentedMeal = async (idMeal) => {
+  const response = await axios.get(`${COMMENTS_API}/${idMeal}/users`);
+  return response.data;
+};
+
+export const findMealsThatUserCommented = async (userId) => {
+  const response = await axios.get(`${USERS_API}/${userId}/comments`);
+  return response.data;
+};
