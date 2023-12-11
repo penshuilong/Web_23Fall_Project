bb47a6738b8aa89968e1e42db75deeb23de747fd
zihanzhao1999 <60196990+zihanzhao1999@users.noreply.github.com>
2023-12-11 03:35:27 -0500

    Update client.js

--- src/project/comments/client.js
+++ src/project/comments/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://project-web23.onrender.com";
+const API_BASE = "https://project-web23.onrender.com/api";
 
 const USERS_API = `${API_BASE}/users`;
 const COMMENTS_API = `${API_BASE}/comments`;
